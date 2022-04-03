b8363044f2aeba60cb50afd2e3f2418c64a8fe0e
Saurabh bisht <Saurabhsid42@gmail.com>
2022-04-03 11:21:23 +0530

    Assignment 1

---
+++ Assignment1/checkAToZ.java
@@ -0,0 +1,26 @@
+package java_Assignment.Assignment1;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class checkAToZ {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print(" Enter a String : ");
+        String input =  sc.next();
+        boolean ans = checkString(input);
+        System.out.println(ans);
+    }
+    public static boolean checkString(String input){
+        int[] arr = new int[26];
+        for(int i=0;i<input.length();i++){
+            char ch = input.charAt(i);
+            arr[ch-'a'] =1;
+        }
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==0)  return false;
+        }
+
+        return true;
+    }
+}
