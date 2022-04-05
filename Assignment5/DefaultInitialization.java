8cb170aac15325db9b754bd087504913c2d16cae
Saurabh bisht <Saurabhsid42@gmail.com>
2022-04-05 08:14:11 +0530

    Assignment 5

---
+++ Assignment5/DefaultInitialization.java
@@ -0,0 +1,38 @@
+package java_Assignment.Assignment5;
+
+public class DefaultInitialization {
+
+    public static void main(String[] args) {
+        Saurabh s=new Saurabh();
+        s.print();
+        //p.printLocal();
+        Singleton s=new Singleton();
+        s.getEmail();
+    }
+}
+
+class Singleton{
+    String email;
+
+    public static Singleton setEmail(String email) {
+        //    this.email = email;
+        return new Singleton();
+    }
+
+    public void getEmail() {
+        System.out.println(this.email);
+    }
+}
+class Saurabh{
+    Character gender;
+    int age;
+    public void print(){
+        System.out.print(this.age+" "+this.gender);
+    }
+//    public void printlocal(){
+//     int height ;
+//        int weight;
+//        System.out.println(height+" "+weight);
+//    }
+    //we cannot print local variables without initializing them;
+}
